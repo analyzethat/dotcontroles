27e73ef43247f1cbf90dbb158edbe442a09b6ea7
Galina Slavova <galina@crafity.com>
2014-01-13 18:33:44 +0100

    Refactored / renamed namespacing of app

--- file_server/content/js/index.js
+++ file_server/content/js/index.js
@@ -1,85 +1,61 @@
 /*globals superagent, window, document, console, moment, numeral, crafity, keyboard, Element, Repository, ConstateringenView, MenuPanel, MenuItem, controles */
-"use strict";
 
-var appContainer;
+(function (controles) {
 
-var app = {
-	initialize: function () {
-		// set the local region
-		numeral.language("be-nl");
-		moment.lang("nl");
+	"use strict";
 
-		console.element = document.querySelector(".console");
+	controles.app = {
 
-		// BEGIN only for testing purposes
-		window.getUser = function () {
-			// test login
-			superagent
-				.get("http://data.dotcontroles.dev/user/1")
-				.end(function (res) {
-					console.log(res);
+		initialize: function () {
+			// set the local region
+			numeral.language("be-nl");
+			moment.lang("nl");
+
+			controles.eventbus = new crafity.core.EventEmitter();
+			
+			console.element = document.querySelector(".console");
+
+			var showLogin, showApp;
+
+			// BEGIN only for testing purposes
+			window.getUser = function () {
+				// test login
+				superagent
+					.get("http://data.dotcontroles.dev/user/1")
+					.end(function (res) {
+						console.log(res);
+					});
+			};
+
+			showLogin = function () {
+				var loginView = new controles.LoginView();
+				console.log("loginView", loginView);
+				document.body.appendChild(loginView.render());
+				window.loginView = loginView;
+				loginView.focus();
+				loginView.on("loggedin", function (user) {
+					document.body.removeChild(loginView.getElement());
+					showApp(user);
 				});
-		};
+				return loginView;
+			};
 
-		function showLogin() {
-			var loginView = new controles.LoginView();
-			console.log("loginView", loginView);
-			document.body.appendChild(loginView.render());
-			window.loginView = loginView;
-			loginView.focus();
-			loginView.on("loggedin", function (user) {
-				document.body.removeChild(loginView.getElement());
-				showApp(user);
-			});
-			return loginView;
+			showApp = function (user) {
+				var appView = new controles.AppView();
+				console.log("appView", appView, user);
+				document.body.appendChild(appView.render());
+				appView.on("logout", function () {
+					document.body.removeChild(appView.getElement());
+					showLogin();
+				});
+				return appView;
+			};
+
+			//showLogin();
+			showApp({});
+
 		}
-		
-		function showApp(user) {
-			var appView = new controles.AppView();
-			console.log("appView", appView);
-			document.body.appendChild(appView.render());
-			appView.on("logout", function () {
-				document.body.removeChild(appView.getElement());
-				showLogin();
-			});			
-			return appView;
-		}
-		
-		//showLogin();
-		showApp();
-		
-//		var loginView = new controles.LoginView();
-//		var appView = new controles.AppView();
-//		appView.render();
-//		
-//		document.body.appendChild(loginView.render());
-//
-//		loginView.on("loggedin", function (user) {
-//
-//			// Remove login view
-//			document.body.removeChild(loginView.getElement());
-//
-//			// Show the main application
-//			document.body.appendChild(appView.getElement());
-//			
-//			appView.on("logout", function () {
-//				document.body.removeChild(appView.getElement());
-//				document.body.appendChild(loginView.getElement());
-//				
-//			});
-//		});
 
-	}
-};
+	};
 
-var console1 = {
-	console: window.console,
-	element: null,
-	log: function () {
-		var args = Array.prototype.slice.call(arguments);
-		args.forEach(function (arg) {
-			console.element.innerText += arg.toString() + " ";
-		});
-		console.element.innerText += "\n";
-	}
-};
+}(window.controles = window.controles || {}));
