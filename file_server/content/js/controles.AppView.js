1af6886ccad45dce6433907394b81802b8533da8
Galina Slavova <galina@crafity.com>
2014-01-13 18:35:56 +0100

    Added a simple event bus to provide the events from different views within controles.app

--- file_server/content/js/controles.AppView.js
+++ file_server/content/js/controles.AppView.js
@@ -9,24 +9,24 @@
 		var URL_DATASERVER = "http://data.dotcontroles.dev";
 
 		var specialistsRepository = new controles.repositories.SpecialistsRepository(superagent, URL_DATASERVER);
-		
-		var constateringenRepository = new controles.repositories.ConstateringenRepository(specialistsRepository);
-		var constateringenView = new crafity.controles.ConstateringenView(constateringenRepository, specialistsRepository);
+
+		var constateringenRepository = null;
+		var constateringenView = null;
+		var controlesRepository = new controles.repositories.ControlesRepository();
+		var controlesView = new controles.views.ControlesView(controlesRepository);
 		var menu = new crafity.html.Menu().addClass("main").appendTo(this);
 		var viewContainer = new crafity.html.ViewContainer().addClass("app").appendTo(this);
 		var userView = new crafity.controles.UserView(new crafity.controles.Repository());
 
-//		appContainer = new crafity.html.Element("div").addClass("app");
-
 		menu.addMenuPanel(new crafity.html.MenuPanel("Overzicht")
 			.addMenuItems([
-				new crafity.html.MenuItem("Constateringen", function () {
-					viewContainer.activate(constateringenView);
+				new crafity.html.MenuItem("DOT Controles", function () {
+					viewContainer.activate(controlesView);
 				}).select()
 			]));
 
 		menu.addMenuPanel(new crafity.html.MenuPanel("Systeem").addMenuItems([
-			
+
 			new crafity.html.MenuItem("Mijn gegevens", function () {
 				viewContainer.activate(userView);
 			}),
@@ -35,6 +35,19 @@
 			})
 		]));
 
+		controles.eventbus.on("openConstateringen", function (controleId) {
+			console.log("Open constateringen", controleId);
+			constateringenRepository = new controles.repositories.ConstateringenRepository(specialistsRepository);
+			constateringenView = new controles.views.ConstateringenView(constateringenRepository, specialistsRepository);
+			viewContainer.activate(constateringenView);
+		});
+		
+		controles.eventbus.on("openControles", function () {
+			console.log("Open controles");
+			viewContainer.activate(controlesView);
+		});
+		
+		
 //		appContainer.toggleClass("fullscreen");
 
 		crafity.keyboard.on("cmd+shft+m", function (e) {
