d228a225d8d17b4f1013483d23569571f41238ab
Galina Slavova <galina@crafity.com>
2014-01-28 23:58:24 +0100

    Refurbishing

--- file_server/content/js/controles.views.UserView.js
+++ file_server/content/js/controles.views.UserView.js
@@ -6,40 +6,88 @@
 
 	(function (views) {
 
-		function UserView(repository) {
-			if (!repository) {
-				throw new Error("Missing argument repository");
+		function UserView(usersRepository) {
+			if (!usersRepository) {
+				throw new Error("Missing argument 'usersRepository'");
 			}
 
 			var self = this;
-			var user = repository.user;
+			var authenticatedUser = null; //usersRepository.authenticatedUser();
 
-			console.log("repository.user", repository.user);
+			console.log("\n inside UserView ", authenticatedUser);
 			// build the GUI elements
 			var infoRow = new html.Form().addClass("clientDetails readonly");
-			var txtUsername = new html.TextField().label("gebruikersnaam").appendTo(infoRow).readonly(true).value(user.username);
-			var txtName = new html.TextField().label("Naam").appendTo(infoRow).value(user.firstName);
-			var txtFamilyName = new html.TextField().label("Achternaam").appendTo(infoRow).value(user.lastName);
+			var txtUsername = new html.TextField().label("Gebruikersnaam").appendTo(infoRow).readonly(true);
+			var txtName = new html.TextField().label("Naam").appendTo(infoRow);
+			var txtFamilyName = new html.TextField().label("Achternaam").appendTo(infoRow);
+			var txtEmail = new html.TextField().label("Email").appendTo(infoRow);
+			var roleContainer = new html.Element("div").appendTo(infoRow);
+			var specialismsContainer = new html.Element("div").appendTo(infoRow);
 
-//			repository.user.get(function (err, user) {
-//
-//				if (err) {
-//					console.log("err", err);
-//				} else {
-////					console.log("Got user: ", user);
-//					txtUsername.value(user.username);
-//					txtName.value(user.firstName);
-//					txtFamilyName.value(user.lastName);
-//				}
-//			});
+			// data-binding
+			controles.eventbus.on("authenticated", function (user) {
+				authenticatedUser = user;
 
+				txtUsername.value(authenticatedUser.Username);
+				txtName.value(authenticatedUser.FirstName);
+				txtFamilyName.value(authenticatedUser.LastName);
+				txtEmail.value(authenticatedUser.Email);
+
+				roleContainer.clear();
+				specialismsContainer.clear();
+				
+				var roleCounter = 0;
+				authenticatedUser.Roles.forEach(function (role) {
+					roleCounter++;
+					new html.TextField().label("Rol " + roleCounter).appendTo(roleContainer).readonly(true).value(role.Name);
+				});
+
+				var specialismCounter = 0;
+				authenticatedUser.Specialisms.forEach(function (specialism) {
+					specialismCounter++;
+					new html.TextField().label("Specialisme " + specialismCounter).appendTo(specialismsContainer).readonly(true).value(specialism.Name);
+				});
+			});
+
+//			Email: 'galina@crafity.com',
+//						Roles: [
+//							{
+//								Id: 1,
+//								UserId: 1,
+//								FunctionalRoleId: 1,
+//								Name: "Specialist",
+//								NeedsSpecialism: true,
+//								CreationDate: "2014-01-14 00:00:00",
+//								LastMutationDate: null
+//							},
+//							{
+//								Id: 2,
+//								UserId: 1,
+//								FunctionalRoleId: 3,
+//								Name: "Zorgadministratie",
+//								NeedsSpecialism: false,
+//								CreationDate: "2014-01-14 00:00:00",
+//								LastMutationDate: null
+//							}
+//						],
+//						Specialisms: [
+//							{
+//								Id: 2,
+//								UserId: 1,
+//								SpecialismId: 9,
+//								Name: "Dermatologie",
+//								AGBCode: "0310",
+//								CreationDate: "2014-01-14 00:00:00",
+//								LastMutationDate: null
+//							}]
+//			
 			this.addClass("detailsContainer");
 			this.save = function () {
-				if (!user || user === null) {
+				if (!authenticatedUser || authenticatedUser === null) {
 					return;
 				}
 
-				repository.user.save(txtName.value(), txtFamilyName.value());
+				usersRepository.user.save(txtName.value(), txtFamilyName.value());
 			};
 
 			var buttonBar = new html.ButtonBar()
