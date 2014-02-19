7bc5d8e6b042981eec2b8f168a3622bdfcc38df7
Galina Slavova <galina@crafity.com>
2014-02-19 19:40:49 +0100

    Updated the paths in run.sh so they are generic for all environments

--- run.sh
+++ run.sh
@@ -2,11 +2,12 @@
 
 sudo killall -9 node
 
-cd /Users/gasl/Development/Crafity/utils/crafity-proxy
+#cd /Users/gasl/Development/Crafity/utils/crafity-proxy
+cd ~/Development/Crafity/utils/crafity-proxy
 sudo node proxy.js &
 
-cd /Users/gasl/Development/Crafity/clients/Analyze\ That/poc_controles/src/file_server
+cd ~/Development/Crafity/clients/Analyze\ That/poc_controles/src/file_server
 /usr/local/bin/nodemon fileserver.js &
 
-cd /Users/gasl/Development/Crafity/clients/Analyze\ That/poc_controles/src/data_server
+cd ~/Development/Crafity/clients/Analyze\ That/poc_controles/src/data_server
 /usr/local/bin/nodemon dataserver.js &
